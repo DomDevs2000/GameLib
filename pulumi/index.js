5543b78a9e144714ddcfc0031fadc892ba2e6658
DomDevs2000 <aidan@carvalho.org.uk>
2023-04-23 13:29:54 +0100

    created ec2 instance instead of s3 bucket

--- pulumi/index.js
+++ pulumi/index.js
@@ -1,54 +1,42 @@
-"use strict";
 const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
 const fs = require('fs');
 
-// Logger bucket
-const logBucket = new aws.s3.Bucket("logBucket", {acl: "log-delivery-write"});
-
-
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("DomDevs-GameLib", {
-    acl: "public-read",
-    policy: {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "PublicReadGetObject",
-                "Effect": "Allow",
-                "Principal": "*",
-                "Action": "s3:GetObject",
-                "Resource": "arn:aws:s3:::domdevs-gamelib-d10aacf/*"
-            }
-        ]
-    },
-    website: {
-        indexDocument: "index.html",
-        errorDocument: "index.html",
-        routingRules: `[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "documents/"
-    }
-}]
-`,
-    },
-    versioning: {
-        enabled: true,
-    },
-    loggings: [{
-        targetBucket: logBucket.id,
-        targetPrefix: "log/",
-    }],
-
-
+const ubuntu = aws.ec2.getAmi({
+    mostRecent: true,
+    filters: [
+        {
+            name: "name",
+            values: ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"],
+        },
+        {
+            name: "virtualization-type",
+            values: ["hvm"],
+        },
+    ],
+    owners: ["099720109477"],
 });
-
-// metric bucket
-const example_entire_bucket = new aws.s3.BucketMetric("metric-entire-bucket", {bucket: bucket.id});
-
-// Export the name of the bucket
-exports.bucketName = bucket.id;
+const web = new aws.ec2.Instance("web", {
+    ami: ubuntu.then(ubuntu => ubuntu.id),
+    instanceType: "t2.micro",
+    tags: {
+        Name: "GameLib",
+    },
+});
+const mainvpc = new aws.ec2.Vpc("mainvpc", {cidrBlock: "10.1.0.0/16"});
+const _default = new aws.ec2.DefaultSecurityGroup("default", {
+    vpcId: mainvpc.id,
+    ingress: [{
+        protocol: "-1",
+        self: true,
+        fromPort: 0,
+        toPort: 0,
+    }],
+    egress: [{
+        fromPort: 0,
+        toPort: 0,
+        protocol: "-1",
+        cidrBlocks: ["0.0.0.0/0"],
+    }],
+});
